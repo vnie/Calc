4b51962df7bbddb2bfefea9a20c7a984a7c8b090
Victor Nie <vnie@uci.edu>
2015-11-10 14:29:35 -0800

    Added basic operations.

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -23,6 +23,47 @@ class ViewController: UIViewController {
         }
     }
     
+    var operandStack: Array<Double> = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+        operandStack.append(displayValue)
+        print("\(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        
+        switch operation {
+        case "×": performOperation({ $0 * $1 })
+        case "÷": performOperation({ $0 / $1 })
+        case "+": performOperation({ $0 + $1 })
+        case "−": performOperation({ $0 - $1 })
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+                enter()
+        }
+
+    }
 }
 
  
